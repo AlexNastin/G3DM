9cf52708225c38646f9c5701adb4ab1590339232
AlexNastin <cabaka@mail.ru>
2015-04-05 13:32:03 +0300

    Domain create Alex

---
+++ src/main/java/com/global3Dmod/printer/domain/Сountry.java
@@ -0,0 +1,75 @@
+package com.global3Dmod.printer.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "—ountries")
+public class —ountry implements Essence {
+
+	@Id
+	@Column(name = "id—ountry")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id—ountry;
+
+	@Column(name = "title")
+	private String title;
+
+	public —ountry() {
+		super();
+	}
+
+	public int getId—ountry() {
+		return id—ountry;
+	}
+
+	public void setId—ountry(int id—ountry) {
+		this.id—ountry = id—ountry;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id—ountry;
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		—ountry other = (—ountry) obj;
+		if (id—ountry != other.id—ountry)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "—ountry [id—ountry=" + id—ountry + ", title=" + title + "]";
+	}
+
+}

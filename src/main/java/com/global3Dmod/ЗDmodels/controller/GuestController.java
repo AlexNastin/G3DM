942cf40065be0a7f6542a55bd6281808f4cb3ce4
AlexNastin <cabaka@mail.ru>
2015-05-02 20:42:26 +0300

    Little update authorization

--- src/main/java/com/global3Dmod/ЗDmodels/controller/GuestController.java
+++ src/main/java/com/global3Dmod/ЗDmodels/controller/GuestController.java
@@ -1,8 +1,11 @@
 package com.global3Dmod.ÇDmodels.controller;
 
+import java.security.Principal;
 import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +23,17 @@ public class GuestController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView index(Locale locale, Model model) throws Exception {
+		
 		ModelAndView modelAndView = new ModelAndView("redirect:/index");
 		return modelAndView;
 	}
 
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public ModelAndView main(Locale locale, Model model) throws Exception {
+		Authentication auth = SecurityContextHolder.getContext()
+				.getAuthentication();
+		String name = auth.getName(); // get logged in username
+		System.out.println("AAAAAAAAAAa  " + name);
 		ModelAndView modelAndView = new ModelAndView("main");
 		modelAndView.addObject("listCategory",
 				guestService.getAllCategoriesSubcategoriesTop3());
@@ -53,13 +61,13 @@ public class GuestController {
 		ModelAndView modelAndView = new ModelAndView("login/signin");
 		return modelAndView;
 	}
-	
+
 	@RequestMapping(value = "/about", method = RequestMethod.GET)
 	public ModelAndView about(Locale locale, Model model) throws Exception {
 		ModelAndView modelAndView = new ModelAndView("about_us");
 		return modelAndView;
 	}
-	
+
 	@RequestMapping(value = "/contact", method = RequestMethod.GET)
 	public ModelAndView contact(Locale locale, Model model) throws Exception {
 		ModelAndView modelAndView = new ModelAndView("contact");
@@ -71,16 +79,41 @@ public class GuestController {
 		ModelAndView modelAndView = new ModelAndView("designer/designer");
 		return modelAndView;
 	}
+
 	@RequestMapping(value = "/u", method = RequestMethod.GET)
 	public ModelAndView u(Locale locale, Model model) throws Exception {
 		ModelAndView modelAndView = new ModelAndView("user/user");
 		return modelAndView;
 	}
-	
+
 	@RequestMapping(value = "/a", method = RequestMethod.GET)
 	public ModelAndView a(Locale locale, Model model) throws Exception {
 		ModelAndView modelAndView = new ModelAndView("admin/admin");
 		return modelAndView;
 	}
-	
+
+	@RequestMapping("user")
+	public ModelAndView geUserPage() {
+		ModelAndView modelAndView = new ModelAndView("user/user");
+		return modelAndView;
+	}
+
+	@RequestMapping("admin")
+	public ModelAndView geAdminPage() {
+		ModelAndView modelAndView = new ModelAndView("admin/admin");
+		return modelAndView;
+	}
+
+	@RequestMapping("designer")
+	public ModelAndView geDesignerPage() {
+		ModelAndView modelAndView = new ModelAndView("designer/designer");
+		return modelAndView;
+	}
+
+	@RequestMapping("403page")
+	public ModelAndView ge403denied() {
+		ModelAndView modelAndView = new ModelAndView("error/403page");
+		return modelAndView;
+	}
+
 }
--- src/main/webapp/WEB-INF/views/user/user.jsp
+++ src/main/webapp/WEB-INF/views/user/user.jsp
@@ -8,7 +8,7 @@
 <html>
 <head>
  <meta charset="utf-8">
-<title>Insert title here</title>
+<title>User</title>
 </head>
 <body>
 

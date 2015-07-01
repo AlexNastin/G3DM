3481eb320f31a727e27e9fe972250018e8e2fa6c
CreaRec <stepanovnickita1994@gmail.com>
2015-07-01 13:17:41 +0300

    исправлена сортировка по статусу

--- src/main/java/com/global3Dmod/ЗDmodels/sort/post/SortedPostsByStatus.java
+++ src/main/java/com/global3Dmod/ЗDmodels/sort/post/SortedPostsByStatus.java
@@ -8,15 +8,16 @@ public class SortedPostsByStatus implements Comparator<Post>{
 
 	@Override
 	public int compare(Post obj1, Post obj2) {
-//		int status1 = obj1.getIsDisplay();
-//        int status2 = obj2.getIsDisplay();
-//        if(status1==1 && status2==false) {
-//            return 1;
-//        }
-//        else if(status1==false && status2==true) {
-//            return -1;
-//        }
-//        else {
+		int status1 = obj1.getIsDisplay();
+        int status2 = obj2.getIsDisplay();
+        if(status2 > status1) {
+            return 1;
+        }
+        else if(status2 < status1) {
+            return -1;
+        }
+        else {
             return 0;
         }
+	}
 }
--- src/main/java/com/global3Dmod/ЗDmodels/sort/post/SortedPostsByStatusDesc.java
+++ src/main/java/com/global3Dmod/ЗDmodels/sort/post/SortedPostsByStatusDesc.java
@@ -8,15 +8,16 @@ public class SortedPostsByStatusDesc implements Comparator<Post>{
 
 	@Override
 	public int compare(Post obj1, Post obj2) {
-//		int status1 = obj1.getIsDisplay();
-//        int status2 = obj2.getIsDisplay();
-//        if(status1==1 && status2==false) {
-//            return 1;
-//        }
-//        else if(status1==false && status2==true) {
-//            return -1;
-//        }
-//        else {
+		int status1 = obj1.getIsDisplay();
+        int status2 = obj2.getIsDisplay();
+        if(status1 > status2) {
+            return 1;
+        }
+        else if(status1 < status2) {
+            return -1;
+        }
+        else {
             return 0;
         }
+	}
 }

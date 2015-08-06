f436077a982731079d1cc46cbfd971d01639fd58
AlexNastin <cabaka@mail.ru>
2015-08-06 16:37:28 +0300

    handle exception 405 and FileNotFoundExc

--- src/main/java/com/global3Dmod/ЗDmodels/controller/GlobalExceptionController.java
+++ src/main/java/com/global3Dmod/ЗDmodels/controller/GlobalExceptionController.java
@@ -1,11 +1,16 @@
 package com.global3Dmod.ÇDmodels.controller;
 
+import java.io.FileNotFoundException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.global3Dmod.ÇDmodels.exception.ServiceException;
 
+@Controller
 @ControllerAdvice
 public class GlobalExceptionController {
 
@@ -15,10 +20,21 @@ public class GlobalExceptionController {
 		return modelAndView;
 	}
 
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	public ModelAndView handleError405() {
+		ModelAndView modelAndView = new ModelAndView("error/error");
+		return modelAndView;
+	}
+
 	@ExceptionHandler(RuntimeException.class)
 	public ModelAndView handleResourceException() {
 		ModelAndView modelAndView = new ModelAndView("error/error");
 		return modelAndView;
 	}
 
+	@ExceptionHandler(FileNotFoundException.class)
+	public ModelAndView handleFileNotFoundException() {
+		ModelAndView modelAndView = new ModelAndView("error/error");
+		return modelAndView;
+	}
 }

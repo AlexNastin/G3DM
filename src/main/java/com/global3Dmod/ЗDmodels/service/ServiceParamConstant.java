443ab87727c536e42de7855edd7f00b1ca912d2e
CreaRec <stepanovnickita1994@gmail.com>
2015-08-13 21:30:59 +0300

    Закомичено добавление к путю при выводе на страницу

--- src/main/java/com/global3Dmod/ЗDmodels/service/ServiceParamConstant.java
+++ src/main/java/com/global3Dmod/ЗDmodels/service/ServiceParamConstant.java
@@ -11,7 +11,8 @@ public final class ServiceParamConstant {
 	public static final int DEFAULT_COUNT = 0;
 	public static final int DEFAULT_IS_DISPLAY = 2;
 	public static final String DEFAULT_NAME_AVATAR = "avatar.jpg";
-	public static final String PATH_TO_DEFAULT_AVATAR = "resources/images/";
+	public static final String PATH_TO_DEFAULT_AVATAR = "/resources/images/";
+//	public static final String PATH_TO_DEFAULT_AVATAR = "resources/images/";
 	
 	public static String CATEGORY_DESC = "categoryDesc";
 	
